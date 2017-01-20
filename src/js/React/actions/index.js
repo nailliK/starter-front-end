1a2d7e02fdccf05f14a5af4ee81106613189ff4e
Scott Kisloski <scott@spiremedia.com>
2017-01-20 10:03:37 -0700

    cleaning up

--- src/js/React/actions/index.js
+++ src/js/React/actions/index.js
@@ -4,22 +4,12 @@ import {
   INITIALIZE_APP
 } from './types';
 
+// when top level app component initially renders
 export function initializeApp() {
-
+  
   // fade in the body
   const classes = document.querySelector('body').className;
   document.querySelector('body').className += (classes.length) ? ' body--loaded' : 'body--loaded';
 
-  return (dispatch, getStore) => {
-    // // GET all existing creatives
-    // axios.get('../../assets/json/creatives.json').then((results) => {
-    //   dispatch({
-    //     type: GET_CREATIVES,
-    //     payload: results.data.Creatives
-    //   });
-    // })
-    // .catch((err) => {
-    //   throw (err);
-    // });
-  };
+  return () => {};
 }
--- src/js/React/reducers/todos.reducer.js
+++ src/js/React/reducers/todos.reducer.js
@@ -1,24 +1,9 @@
-import {
-  SET_LOADING
-} from '../actions/types';
+import {} from '../actions/types';
 
-const paginationLimit = 40;
-
-const INITIAL_STATE = {
-  isLoading: 0
-};
+const INITIAL_STATE = {};
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
-
-    // set loading state
-    case SET_LOADING: {
-      let isLoading = state.isLoading;
-      action.payload === 'INCREMENT' ? isLoading++ : isLoading--;
-      return Object.assign({}, state, {
-        isLoading: isLoading
-      });
-    }
     
     default:
       return state;
