c651df772aeeaa9de31483d9f2b1c908d4933590
Killian Grant <Killian@Killians-MacBook-Pro.local>
2017-02-13 15:29:52 -0700

    update

--- src/js/Vue/components/app.vue
+++ src/js/Vue/components/app.vue
@@ -1,25 +1,25 @@
 <style lang="scss">
-	/* all necessary mixins, frameworks and variables */
-	@import 'includes';
+    /* all necessary mixins, frameworks and variables */
+    @import 'includes';
 
-	/* Global styles (only include this up in app.vue) */
-	@import '_reset';
-	@import 'font-awesome/_font-awesome';
-	@import 'global';
+    /* Global styles (only include this up in app.vue) */
+    @import '_reset';
+    @import 'font-awesome/_font-awesome';
+    @import 'global';
 </style>
 
 <template>
-	<wrapper>
-		<navigation></navigation>
-		<router-view></router-view>
-	</wrapper>
+    <wrapper>
+        <navigation></navigation>
+        <router-view></router-view>
+    </wrapper>
 </template>
 
 <script>
-	export default {
-		name: 'app',
-		components: {
-			navigation: require('./navigation.vue')
-		}
-	}
+    export default {
+        name: 'app',
+        components: {
+            navigation: require('./navigation.vue')
+        }
+    }
 </script>
